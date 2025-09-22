c527d20985d8339c1c77daf823e5cc11fa756945
pdemeulenaer <pdemeulenaer@gmail.com>
2025-09-22 11:54:29 +0300

    remove comments in retrieval.py

--- src/api/rag/retrieval.py
+++ src/api/rag/retrieval.py
@@ -173,9 +173,7 @@ def retrieve_context(query, qdrant_client, top_k=5):
                 filter=Filter(
                     must=[
                         FieldCondition(
-                            key="text",
-                            match=MatchText(text=query)
-                        )
+                            key="text", match=MatchText(text=query)),
                     ]
                 ),
                 limit=20
@@ -256,50 +254,6 @@ def process_context(context):
         lines.append(f"\n\n DOC ID: {id}; DOC TEXT: {chunk}\n\n---")
     return "\n".join(lines)
 
-
-# OUTPUT_SCHEMA = {
-#     "type": "object",
-#     "properties": {
-#         "answer": {
-#             "type": "string",
-#             "description": "The answer to the question based on the provided context.",
-#         },
-#         "retrieved_context_ids": {
-#             "type": "array",
-#             "items": {
-#                 "type": "object",
-#                 "properties": {
-#                     "id": {
-#                         "type": "string", #"integer",
-#                         "description": "The uuid index of the chunk that was used to answer the question.",
-#                     },
-#                     "description": {
-#                         "type": "string",
-#                         "description": "Short description of the item based on the context together with the id.",
-#                     },
-#                 },
-#             },
-#         },
-#     },
-# }
-
-# OUTPUT_SCHEMA = {
-#     "type": "object",
-#     "properties": {
-#         "answer": {
-#             "type": "string",
-#             "description": "The answer to the question based on the provided documentation."
-#         },
-#         "retrieved_context_ids": {
-#             "type": "array",
-#             "items": {
-#                 "type": "string",
-#                 "description": "UUID of a document chunk that was used to answer the question."
-#             }
-#         },
-#     },
-#     "required": ["answer", "retrieved_context_ids"]
-# }
 
 OUTPUT_SCHEMA = {
     "type": "object",
@@ -467,92 +421,6 @@ def generate_answer(prompt, generation_model=None):
         current_run.metadata["usage_metadata"] = usage
 
     return response
-
-# def generate_answer(prompt, generation_model=None):
-
-#     # client = instructor.from_openai(OpenAI(api_key=config.OPENAI_API_KEY))
-#     # response, raw_response = client.chat.completions.create_with_completion(
-#     #     model="gpt-4.1", #"gpt-5-mini", # "gpt-4.1", #
-#     #     response_model=RAGGenerationResponse,
-#     #     messages=[{"role": "user", "content": prompt}],
-#     #     temperature=0.5,
-#     # )
-
-#     # Call OpenAI with JSON output enforcement
-#     client = OpenAI(api_key=config.OPENAI_API_KEY)
-#     response_json = client.chat.completions.create(
-#         model=generation_model, #config.GENERATION_MODEL, # "gpt-4.1",
-#         # messages=[
-#         #     {"role": "system", "content": "You are a helpful assistant."},
-#         #     {"role": "user", "content": "Answer using JSON format."}
-#         # ],
-#         messages=prompt,
-#         # temperature=0,
-#         # max_tokens=1024,
-#         # response_format={"type": "json_schema", "json_schema": OUTPUT_SCHEMA}
-#         response_format={
-#             "type": "json_schema",
-#             "json_schema": {
-#                 "name": "RAGGenerationResponse",
-#                 "schema": OUTPUT_SCHEMA
-#             }
-#         }
-#     )
-
-#     raw_content = response_json.choices[0].message.content
-#     parsed_content = json.loads(raw_content)  # now it's a dict
-
-#     # Parse the JSON into your Pydantic model
-#     response = RAGGenerationResponse(**parsed_content)    
-
-#     current_run = get_current_run_tree()
-#     if current_run:
-#         current_run.metadata["usage_metadata"] = {
-#             "input_tokens": response_json.usage.prompt_tokens,
-#             "output_tokens": response_json.usage.completion_tokens,
-#             "total_tokens": response_json.usage.total_tokens,
-#         }
-
-#     return response
-
-
-# @traceable(
-#     name="generate_answer",
-#     run_type="llm",
-#     metadata={"ls_provider": config.GENERATION_MODEL_PROVIDER, "ls_model_name": config.GENERATION_MODEL}
-# )
-# def generate_answer_groq(prompt, generation_model=None):    
-
-#     # Initialize the Groq client
-#     groq_client = Groq(api_key=config.GROQ_API_KEY)
-
-#     # Patch the Groq client with instructor
-#     client = instructor.from_groq(groq_client)
-
-#     logger.info(f"-----")
-#     logger.info(f"Prompt length: {len(prompt)}")
-#     logger.info(f"Prompt: {prompt}")
-#     logger.info(f"-----")
-
-#     # Use the instructor-patched Groq client for chat completions
-#     response, raw_response = client.chat.completions.create_with_completion(
-#         model=generation_model, # config.GENERATION_MODEL, # "llama-3.3-70b-versatile",
-#         response_model=RAGGenerationResponse,
-#         messages=prompt, #[{"role": "user", "content": prompt}],
-#         temperature=0, #config.GENERATION_MODEL_TEMPERATURE, #0.5,
-#         max_tokens=config.GENERATION_MODEL_MAX_TOKENS, #1024
-#         max_retries=5,  # Set the number of retries here
-#     )
-
-#     current_run = get_current_run_tree()
-#     if current_run:
-#         current_run.metadata["usage_metadata"] = {
-#             "input_tokens": raw_response.usage.prompt_tokens,
-#             "output_tokens": raw_response.usage.completion_tokens,
-#             "total_tokens": raw_response.usage.total_tokens,
-#         }
-
-#     return response
 
 
 @traceable(
